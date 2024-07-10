7b633fccb94703f6f1434a76373ea1ae070f2c9c
annaj <ajo3238@wgu.edu>
2024-07-10 11:23:34 -0400

    I had to fix some of the items to go to the correct spot.

---
+++ src/test/java/capstone/code/controllers/MainScreenControllerrTest.java
@@ -0,0 +1,12 @@
+package capstone.code.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainScreenControllerrTest {
+
+    @Test
+    void mainScreen() {
+    }
+}
